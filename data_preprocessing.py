3296bb92b58e3b05adb64886e8229a9138d68b9a
Emaduwage Thaveesha Urvinda <thaveeshaurvinda2007@gmail.com>
2026-06-10 11:51:42 +0530

    Implemented data splitting and scaling

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -62,4 +62,82 @@ def handle_outliers_iqr(data, target_column = "Appliances"):
     return cleaned_data
 
 
+# Split data and scale
+def split_and_scale(data, target_column = "Appliances", train_ratio = 0.8):
+    """
+    1. Splits data into training and testing sets
+    2. Keeps chronological order
+    3. Applies Min-Max scaling
+    """
+    # Columns that should not be used as features (inputs)
+    columns_to_remove = ["date", target_column, "WeekStatus"]
+
+    feature_columns = []
+    for column in data.columns:
+        if column not in columns_to_remove:
+            feature_columns.append(column)
+
+    # Calculate split position
+    split_index = int(len(data) * train_ratio)
+
+    # Training data = first 80%
+    train_data = data.iloc[:split_index].copy()
+    # Testing data = remaining 20%
+    test_data = data.iloc[split_index:].copy()
+
+    # Create scaler for features (X)
+    feature_scaler = MinMaxScaler(feature_range=(0, 1)) # Min 0, Max 1
+    # Create scaler for target (Y)
+    target_scaler = MinMaxScaler(feature_range=(0, 1))  # Min 0, Max 1
+
+    # Scale feature columns
+    # Learn scaling values from training data
+    train_features_scaled = feature_scaler.fit_transform(
+        train_data[feature_columns]
+    )
+    # Apply scaling to test data
+    test_features_scaled = feature_scaler.transform(
+        test_data[feature_columns]
+    )
+
+    # Scale target column
+    train_target_scaled = target_scaler.fit_transform(
+        train_data[[target_column]]
+    )
+    test_target_scaled = target_scaler.transform(
+        test_data[[target_column]]
+    )
+
+    # Create scaled training dataframe
+    scaled_train = pd.DataFrame(
+        train_features_scaled,
+        columns = feature_columns,
+        index = train_data.index
+    )
+
+    # Add scaled target column
+    scaled_train[target_column] = train_target_scaled
+    # Add date column back
+    scaled_train["date"] = train_data["date"]
+
+    # Create scaled testing dataframe
+    scaled_test = pd.DataFrame(
+        test_features_scaled,
+        columns = feature_columns,
+        index = test_data.index
+    )
+
+    # Add scaled target column
+    scaled_test[target_column] = test_target_scaled
+    # Add date column back
+    scaled_test["date"] = test_data["date"]
+
+    # Return all outputs
+    return (
+        scaled_train,
+        scaled_test,
+        feature_scaler,
+        target_scaler,
+        feature_columns
+    )
 
